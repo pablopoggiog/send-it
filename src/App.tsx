5e6d8fd610be9dbbd26dffa57132e9a18f09ab7b
Pablo Poggio <pablopoggiog@gmail.com>
2025-08-27 21:11:06 -0300

    Update error toast duration and delete useless test

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ export function App() {
               minWidth: "300px"
             },
             success: {
-              duration: 15000,
+              duration: 10000,
               style: {
                 background: "rgba(16, 185, 129, 0.1)",
                 border: "1px solid rgba(16, 185, 129, 0.2)",
